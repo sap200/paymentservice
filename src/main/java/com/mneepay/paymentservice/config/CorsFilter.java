2daa07b872187818ea7cc50bd879e8fc2dbd26d5
sapta <www.saptarsi@gmail.com>
2026-01-02 21:54:30 +0100

    updated cors Filter

--- src/main/java/com/mneepay/paymentservice/config/CorsFilter.java
+++ src/main/java/com/mneepay/paymentservice/config/CorsFilter.java
@@ -9,10 +9,13 @@ public class CorsFilter implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")  // apply to all endpoints
+                .allowedOrigins(
+                        "https://rapydmnee.netlify.app/" // Replace with your Netlify URL
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // allow standard methods
+                .allowedHeaders("*")   // allow all headers
+                .allowCredentials(true) // allow cookies/auth
+                .maxAge(3600);          // cache preflight response for 1 hour
     }
 }
